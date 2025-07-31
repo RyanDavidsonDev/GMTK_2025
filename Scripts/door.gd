eed3bce4815ed44c23fce3f206542da41b8ff56b
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-07-31 10:53:01 -0600

    added collision detection and some infrastructure for keys and interaction

--- Scripts/door.gd
+++ Scripts/door.gd
@@ -4,14 +4,18 @@ extends Node
 
 @export var OutwardRotationDeg: float = 90;
 @export var RotationTimeSec: float = .8;
+@export var NeedsKey: bool = false;
 
+@onready var rigid_body_3d: RigidBody3D = $PivotPoint/RigidBody3D
+@onready var collision_shape_3d: CollisionShape3D = %CollisionShape3D
 @onready var pivot_point: Node3D = $PivotPoint
 
 var doorIsOpening : bool = false;
 var doorIsAlreadyOpen : bool = false;
 
 func _ready():
-	print("ready")
+	rigid_body_3d.body_entered.connect(receiveCollision)
+	return
 	
 func _physics_process(_delta):
 	
@@ -19,22 +23,41 @@ func _physics_process(_delta):
 		var NextRotDeg: float = OutwardRotationDeg * inverse_lerp( 0.0, RotationTimeSec,  _delta)
 		pivot_point.rotate_y(deg_to_rad(NextRotDeg))
 	
-		if(rad_to_deg( pivot_point.rotation.y) >= OutwardRotationDeg):
-			pivot_point.rotation.y= (deg_to_rad(OutwardRotationDeg))
+		if( pivot_point.rotation_degrees.y >= OutwardRotationDeg):
+			print("rotation ", pivot_point.rotation_degrees.y)
+			pivot_point.rotation_degrees.y= OutwardRotationDeg
 			doorIsOpening = false 
 			doorIsAlreadyOpen = true
-		#todo: add another check for if we collide against anything like a wall
-		
+
+func receiveCollision(body):
+	print("ping")
+	if(body.is_in_group("BlockDoorTurn")):
+		doorIsAlreadyOpen = true
+		doorIsOpening = false
+
 func resetDoor():
+	print("rotation ", pivot_point.rotation_degrees.y)
 	doorIsAlreadyOpen = false
 	doorIsOpening = false
-	pivot_point.rotation.y= (deg_to_rad(0))
+	pivot_point.rotation_degrees.y= 0
+	#pivot_point.rotate_y(-pivot_point.rotation.y)
+	print("rotation ", pivot_point.rotation_degrees.y)
 	
 
-func open():
+func open() -> void:
+	print("rotation ", pivot_point.rotation_degrees.y)
 	doorIsOpening = true;
 	#root.translate(Vector3(1,1,1))
 	print("received openDoor func")
+
+func interact(player: Node3D) -> void :
+	if(NeedsKey):		
+		assert("HasKey" in player, "ERROR: player param in door.Interact() does not have the HasKey property")
+		if(!player.HasKey):
+			print("you need a key")
+			return
+	
+	open()
 
 
 func _on_button_pressed() -> void:
