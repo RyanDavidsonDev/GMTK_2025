a03a527f847a66629eadb2cc4fc832b35686f873
Ethan Baedke <ethanbaedke@gmail.com>
2025-07-31 10:51:16 -0500

    Rotation with mouse

--- player_character/player_character.gd
+++ player_character/player_character.gd
@@ -2,28 +2,53 @@ class_name PlayerCharacter extends CharacterBody3D
 
 @export var _move_speed : float = 2.0
 
-var _input_direction : Vector2 = Vector2.ZERO
+var _input_move_direction : Vector2 = Vector2.ZERO
+var _input_mouse_direction : Vector2 = Vector2.ZERO
+
+var _camera_3d : Camera3D = null
+
+func _ready() -> void:
+	
+	_camera_3d = $Camera3D
+	
+	# WARNING: This should be handeled in game management
+	Input.set_mouse_mode(Input.MOUSE_MODE_CAPTURED)
+
+func _input(event: InputEvent) -> void:
+	if event is InputEventMouseMotion:
+		_input_mouse_direction = event.relative
 
 func _process(delta: float) -> void:
 
 	# Process movement input
 	
-	_input_direction = Vector2.ZERO
+	_input_move_direction = Vector2.ZERO
 	
 	if Input.is_action_pressed("move_forward"):
-		_input_direction.y += 1.0
+		_input_move_direction.y += 1.0
 	if Input.is_action_pressed("move_backward"):
-		_input_direction.y -= 1.0
+		_input_move_direction.y -= 1.0
 	if Input.is_action_pressed("move_right"):
-		_input_direction.x += 1.0
+		_input_move_direction.x += 1.0
 	if Input.is_action_pressed("move_left"):
-		_input_direction.x -= 1.0
+		_input_move_direction.x -= 1.0
 		
-	_input_direction = _input_direction.normalized()
+	_input_move_direction = _input_move_direction.normalized()
 
 func _physics_process(delta: float) -> void:
 	
-	velocity = (transform.basis.z * -_input_direction.y) + (transform.basis.x * _input_direction.x)
+	# Update character rotation
+	rotation_degrees.y -= _input_mouse_direction.x
+	
+	# Update camera rotation
+	_camera_3d.rotation_degrees.x -= _input_mouse_direction.y
+	_camera_3d.rotation_degrees.x = clampf(_camera_3d.rotation_degrees.x, -90.0, 90.0)
+	
+	_input_mouse_direction = Vector2.ZERO
+	
+	# Handle movement
+	
+	velocity = (transform.basis.z * -_input_move_direction.y) + (transform.basis.x * _input_move_direction.x)
 	velocity *= _move_speed
 	
 	move_and_slide()
