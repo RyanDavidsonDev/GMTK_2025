ebb41d0d44ca161cf6aa6438b3ac8745cfc4c8ef
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-07-31 16:47:22 -0600

    basic interaction linked to door logic, resized door to fit better with player

--- player_character/player_character.gd
+++ player_character/player_character.gd
@@ -1,6 +1,12 @@
 class_name PlayerCharacter extends CharacterBody3D
 
+const INTERACTION_DISTANCE: float = 5
+
 @export var _move_speed : float = 2.0
+@export var _look_sensitivity: float = 1.0
+@export_flags_3d_physics var raycast_collison_mask = 0
+
+@onready var camera: Camera3D = $Camera3D
 
 var _input_move_direction : Vector2 = Vector2.ZERO
 var _input_mouse_direction : Vector2 = Vector2.ZERO
@@ -15,7 +21,7 @@ func _ready() -> void:
 
 func _input(event: InputEvent) -> void:
 	if event is InputEventMouseMotion:
-		_input_mouse_direction = event.relative
+		_input_mouse_direction = event.relative * _look_sensitivity
 
 func _process(delta: float) -> void:
 
@@ -32,9 +38,13 @@ func _process(delta: float) -> void:
 	if Input.is_action_pressed("move_left"):
 		_input_move_direction.x -= 1.0
 		
+		
 	_input_move_direction = _input_move_direction.normalized()
+	
+	
 
 func _physics_process(delta: float) -> void:
+	
 	
 	# Update character rotation
 	rotation_degrees.y -= _input_mouse_direction.x
@@ -51,3 +61,25 @@ func _physics_process(delta: float) -> void:
 	velocity *= _move_speed
 	
 	move_and_slide()
+	
+	
+	#raycast 
+	var space_state = get_world_3d().direct_space_state
+	#camera.get_window().wid
+	var mousepos = get_viewport().get_mouse_position()
+	
+	var origin = camera.project_ray_origin(mousepos)
+	var end = origin + camera.project_ray_normal(mousepos) * INTERACTION_DISTANCE
+	var query = PhysicsRayQueryParameters3D.create(origin, end)
+	query.collide_with_areas = true
+	query.collision_mask = (raycast_collison_mask)
+	var result:Dictionary = space_state.intersect_ray(query)
+	
+	
+	
+	if(!result.is_empty()):
+		print("found something at")
+		
+		var collider:Node3D = result.collider
+		if Input.is_action_just_pressed("Interact"):
+			collider.interact(self)
