731f4af0ed53d49a2ff0455a4856ee63f7889cc9
Ethan Baedke <ethanbaedke@gmail.com>
2025-08-02 17:19:59 -0500

    Dynamo Aux

--- audio/audio_manager.gd
+++ audio/audio_manager.gd
@@ -1,27 +1,21 @@
 extends Node
 
-@export var _killer_default_track : AudioStreamMP3 = null
-@export var _killer_chase_track : AudioStreamMP3 = null
-
+var _killer_surprise_audio_player : AudioStreamPlayer3D = null
 var _killer_default_audio_player : AudioStreamPlayer3D = null
 var _killer_chase_audio_player : AudioStreamPlayer3D = null
 
+var _required_out_of_sight_time_for_reset : float = 10.0
+var _chase_time_remaining : float = 0.0
+
 func initialize_killer_audio(killer: EnemyAI) -> void:
 	
-	# Setup default audio
-	_killer_default_audio_player = AudioStreamPlayer3D.new()
-	killer.add_child(_killer_default_audio_player)
-	_killer_default_audio_player.stream = _killer_default_track
-	_killer_default_audio_player.attenuation_model = AudioStreamPlayer3D.ATTENUATION_DISABLED
-	_killer_default_audio_player.panning_strength = 0.0
-	_killer_default_audio_player.play()
+	_killer_surprise_audio_player = $KillerSurprisePlayer
+	_killer_default_audio_player = $KillerDefaultPlayer
+	_killer_chase_audio_player = $KillerChasePlayer
 	
-	# Setup chase audio
-	_killer_chase_audio_player = AudioStreamPlayer3D.new()
-	killer.add_child(_killer_chase_audio_player)
-	_killer_chase_audio_player.stream = _killer_chase_track
-	_killer_chase_audio_player.attenuation_model = AudioStreamPlayer3D.ATTENUATION_DISABLED
-	_killer_chase_audio_player.panning_strength = 0.0
+	_killer_default_audio_player.play()
+	_killer_chase_audio_player.volume_linear = 0.0
+	_killer_chase_audio_player.play()
 
 func _physics_process(delta: float) -> void:
 	
@@ -30,8 +24,18 @@ func _physics_process(delta: float) -> void:
 	
 	# Default audio is based on "terror radius", pathing distance from the killer to the player
 	var killer_path_distance : float = GameManager.enemy_ai.get_path_distance_from_player()
-	killer_path_distance /= 4.0
-	_killer_default_audio_player.volume_db = -(killer_path_distance * killer_path_distance)
+	_killer_default_audio_player.volume_db = -(killer_path_distance / 2.0)
 	
 	var can_see_player : bool = GameManager.enemy_ai.can_see_player()
-	print(can_see_player)
+	
+	# Killer can see the player
+	if can_see_player:
+		
+		# Currently outside of a chase, begin a chase
+		if _chase_time_remaining <= 0.0:
+			_killer_surprise_audio_player.play()
+		
+		_chase_time_remaining = _required_out_of_sight_time_for_reset
+	
+	_killer_chase_audio_player.volume_linear = max(0.0, _chase_time_remaining) / _required_out_of_sight_time_for_reset
+	_chase_time_remaining -= delta
