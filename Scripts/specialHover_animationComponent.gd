89a82012a895af79330ec56d7790f81328b8d4cb
Fiona Bolen <fionabolen@gmail.com>
2025-08-02 00:50:17 -0600

    Game Over Menu ready for implementation

---
+++ Scripts/specialHover_animationComponent.gd
@@ -0,0 +1,23 @@
+class_name SpecialH_AnimationComponent extends Node
+
+var original_text: String = "Wake Up "
+var hover_text: String = "Wake Up?"
+
+func _ready():
+	
+	var button = get_parent()
+	if button is Button:
+		
+		# Connect hover signals
+		button.mouse_entered.connect(_on_button_mouse_entered)
+		button.mouse_exited.connect(_on_button_mouse_exited)
+	else:
+		push_error("This AnimationComponent needs a better father figure (must be a child of a Button) :pensive emoji:.")
+
+func _on_button_mouse_entered():
+	var button = get_parent() as Button
+	button.text = hover_text
+
+func _on_button_mouse_exited():
+	var button = get_parent() as Button
+	button.text = original_text
