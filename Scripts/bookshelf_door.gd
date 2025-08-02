77187c89652057a206a36e44c471219aee2d9604
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-08-02 16:27:39 -0600

    finished auto bookshelf door opening

--- Scripts/bookshelf_door.gd
+++ Scripts/bookshelf_door.gd
@@ -6,7 +6,7 @@ var insertionOrder: Array[Book] = []
 
 @export var books: Array[Book] = []
 
-@export var open_door_automatically = false;
+@export var open_door_automatically = true;
 
 func _ready():
 	for book in books:
@@ -14,10 +14,10 @@ func _ready():
 
 func receive_book_interact(book:Book):
 	insertionOrder.append(book)
-	if open_door_automatically:
+	if open_door_automatically && insertionOrder.size() >= books.size():
 		if(_check_if_door_can_be_opened(GameManager.player_character)):
 			open()
-			
+		
 	return
 
 func remove_book(book:Book):
