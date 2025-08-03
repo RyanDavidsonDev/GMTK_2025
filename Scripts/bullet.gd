8c8ef51b9561e759289c775d90274afe435d6095
RyanDavidsonDev <ryan.r.davidson.dev@gmail.com>
2025-08-03 05:08:30 -0600

    bullet bob

--- Scripts/bullet.gd
+++ Scripts/bullet.gd
@@ -1,5 +1,12 @@
 extends RigidBody3D
 
+const spin_speed = 1
+const bob_speed = 3
+const bob_height = .2
+var bob_timer = 0
+
+@onready var start_y : float = global_position.y
+
 func interact(player:PlayerCharacter):
 	GameManager.hud_controller.show_next_bullet()
 	player.gun.bullet_count+=1
@@ -7,3 +14,13 @@ func interact(player:PlayerCharacter):
 
 	queue_free()
 	#GameManager.hud_controller.show_text_timer("Press 'R' to reload")
+
+
+func _physics_process(delta: float) -> void:
+	rotate(Vector3.UP, spin_speed*delta)
+	bob_timer += delta
+	
+	var d = (sin(bob_timer * bob_speed) +1)/2
+	global_position.y = start_y + (d*bob_height)
+	
+	
