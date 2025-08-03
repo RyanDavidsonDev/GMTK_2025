7c244d7ebdaf703bc91cffb7e96378dddcec2049
Ethan Baedke <ethanbaedke@gmail.com>
2025-08-02 22:19:15 -0500

    Bookshelf puzzle

--- Scripts/key.gd
+++ Scripts/key.gd
@@ -3,7 +3,7 @@ extends RigidBody3D
 
 func interact(player:PlayerCharacter):
 	if(player.has_key):
-		GameManager.hud_controller.show_text("My hands are full. I can't hold more than one Key")
+		GameManager.hud_controller.show_text_continual("My hands are full. I can't hold more than one Key")
 	else :
 		player.has_key = true;
 		GameManager.hud_controller.show_key()
